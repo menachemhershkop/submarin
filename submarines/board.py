d27ae62d894d9ab98fcbea1d428546a7687e8b40
mendy hershkop <hers.mm@gmail.com>
2025-10-27 17:49:05 +0200

    render public func not finely

--- submarines/board.py
+++ submarines/board.py
@@ -8,13 +8,21 @@ def in_bounds(size: int, x: int, y: int) -> bool:
     return True
 def count_remaining_ships(ships: list[list[int]], shots: list[list[bool]]) -> int:
     shot_count=0
-    for i,j in enumearte(ships):
-        for x,y in emumerate(j):
+    for i,j in enumerate(ships):
+        for x,y in enumerate(j):
             if shots[x][y]==ships[i][j]:
                 shot_count+=1
     return shot_count
 def render_public(ships: list[list[int]], shots: list[list[bool]]) -> str:
-    pass
+    for i, j in enumerate(ships):
+        for x, y in enumerate(j):
+            if shots[x][y] == ships[i][j]:
+                ships[i][j] = "V"
+            if not ships[x][y]==ships[i][j]:
+                ships[i][j]== "X"
+            else:
+                ships[i][j]=="O"
+    return ships
 def render_reveal(ships: list[list[int]], shots: list[list[bool]]) -> str:
     pass
 
